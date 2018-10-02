83aec633019a5dd63be733572cbccefd7969b0c6
Mikael Berglund <mikbe@kth.se>
2018-10-02 23:45:20 +0200

    Small cleanup on clients

--- src/main/java/com/valhallagame/partyserviceclient/PartyServiceClient.java
+++ src/main/java/com/valhallagame/partyserviceclient/PartyServiceClient.java
@@ -1,37 +1,24 @@
 package com.valhallagame.partyserviceclient;
 
-import java.io.IOException;
-
+import com.valhallagame.common.AbstractServiceClient;
 import com.valhallagame.common.DefaultServicePortMappings;
-import com.valhallagame.common.RestCaller;
 import com.valhallagame.common.RestResponse;
-import com.valhallagame.partyserviceclient.message.AcceptParameter;
-import com.valhallagame.partyserviceclient.message.CancelCharacterInviteParameter;
-import com.valhallagame.partyserviceclient.message.DeclineParameter;
-import com.valhallagame.partyserviceclient.message.GetPartyAndInvitesParameter;
-import com.valhallagame.partyserviceclient.message.GetPartyByIdParameter;
-import com.valhallagame.partyserviceclient.message.GetPartyParameter;
-import com.valhallagame.partyserviceclient.message.InviteCharacterParameter;
-import com.valhallagame.partyserviceclient.message.InvitePersonParameter;
-import com.valhallagame.partyserviceclient.message.KickCharacterFromPartyParameter;
-import com.valhallagame.partyserviceclient.message.LeavePartyParameter;
-import com.valhallagame.partyserviceclient.message.PromoteCharacterToLeaderParameter;
+import com.valhallagame.partyserviceclient.message.*;
 import com.valhallagame.partyserviceclient.model.PartyAndInvitesData;
 import com.valhallagame.partyserviceclient.model.PartyData;
 
-public class PartyServiceClient {
+import java.io.IOException;
+
+public class PartyServiceClient extends AbstractServiceClient {
 	private static PartyServiceClient partyServiceClient;
 
-	private String partyServiceServerUrl = "http://localhost:" + DefaultServicePortMappings.PARTY_SERVICE_PORT;
-	private RestCaller restCaller;
-
 	private PartyServiceClient() {
-		restCaller = new RestCaller();
+		serviceServerUrl = "http://localhost:" + DefaultServicePortMappings.PARTY_SERVICE_PORT;
 	}
 
-	public static void init(String partyServiceServerUrl) {
+	public static void init(String serviceServerUrl) {
 		PartyServiceClient client = get();
-		client.partyServiceServerUrl = partyServiceServerUrl;
+		client.serviceServerUrl = serviceServerUrl;
 	}
 
 	public static PartyServiceClient get() {
@@ -42,59 +29,59 @@ public class PartyServiceClient {
 	}
 
 	public RestResponse<String> invitePerson(String senderUsername, String receiverUsername) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/invite-person",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/invite-person",
 				new InvitePersonParameter(senderUsername, receiverUsername), String.class);
 	}
 
 	public RestResponse<String> inviteCharacter(String senderUsername, String characterName) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/invite-character",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/invite-character",
 				new InviteCharacterParameter(senderUsername, characterName), String.class);
 	}
 
 	public RestResponse<String> cancelCharacterInvite(String cancelerUsername, String canceleeCharacterName)
 			throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/cancel-character-invite",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/cancel-character-invite",
 				new CancelCharacterInviteParameter(cancelerUsername, canceleeCharacterName), String.class);
 	}
 
 	public RestResponse<String> acceptInvite(String accpeterUsername, Integer partyId) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/accept-invite",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/accept-invite",
 				new AcceptParameter(accpeterUsername, partyId), String.class);
 	}
 
 	public RestResponse<String> declineInvite(String declinerUsername, Integer partyId) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/decline-invite",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/decline-invite",
 				new DeclineParameter(declinerUsername, partyId), String.class);
 	}
 
 	public RestResponse<String> leaveParty(String leaverUsername) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/leave-party",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/leave-party",
 				new LeavePartyParameter(leaverUsername), String.class);
 	}
 
 	public RestResponse<String> promoteCharacterToLeader(String promoter, String promoteeCharacterName)
 			throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/promote-character-to-leader",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/promote-character-to-leader",
 				new PromoteCharacterToLeaderParameter(promoter, promoteeCharacterName), String.class);
 	}
 
 	public RestResponse<String> kickCharacterFromParty(String kicker, String kickeeCharacterName) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/kick-character-from-party",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/kick-character-from-party",
 				new KickCharacterFromPartyParameter(kicker, kickeeCharacterName), String.class);
 	}
 
 	public RestResponse<PartyData> getParty(String username) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/get-party", new GetPartyParameter(username),
+		return restCaller.postCall(serviceServerUrl + "/v1/party/get-party", new GetPartyParameter(username),
 				PartyData.class);
 	}
 
 	public RestResponse<PartyData> getPartyById(Integer partyId) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/get-party-by-id",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/get-party-by-id",
 				new GetPartyByIdParameter(partyId), PartyData.class);
 	}
 
 	public RestResponse<PartyAndInvitesData> getPartyAndInvites(String username) throws IOException {
-		return restCaller.postCall(partyServiceServerUrl + "/v1/party/get-party-and-invites",
+		return restCaller.postCall(serviceServerUrl + "/v1/party/get-party-and-invites",
 				new GetPartyAndInvitesParameter(username), PartyAndInvitesData.class);
 	}
 
